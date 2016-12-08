34deb8e96cda18130a1d399f9ad8e4fb6fc36ee4
Zhuyifan <Zhuyifan@DESKTOP-B255LCT>
2016-12-08 09:19:34 +0800

    添加项目文件。

---
+++ src/mvcCoreTest/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using System;
+
+namespace mvcCoreTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IMemoryCache _memoryCache;
+
+        public HomeController(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+
+        public IActionResult Index()
+        {
+
+            try
+            {
+                string cacheKey = "key";
+
+                string result;
+                if (!_memoryCache.TryGetValue(cacheKey, out result))
+                {
+                    result = $"哈哈哈{DateTime.Now.AddYears(1)}哈哈哈";
+                    _memoryCache.Set(cacheKey, result);
+                }
+                ViewBag.Cache = result;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return View();
+        }
+            
+    }
+}
